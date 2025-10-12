0dd4131821c99ae9e8c07b9e0136a9aeff1ee99b
nandinigupta-07 <guptanandini072@gmail.com>
2025-10-12 18:57:03 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (96.55%) - LeetSync

--- 450-delete-node-in-a-bst/delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -15,19 +15,19 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-         if(root==null) return null;
-         if(root.val==key){
+        if(root==null) return null;
+        if(root.val==key){
             return helper(root);
-         }
-         TreeNode dummy=root;
-         while(root!=null){
+        }
+        TreeNode dummy=root;
+        while(root!=null){
             if(root.val>key){
-                if(root.left!=null && root.left.val==key){
+                 if(root.left!=null && root.left.val==key){
                     root.left=helper(root.left);
                     break;
-                }else{
-                    root=root.left;
-                }
+                    }else{
+                        root=root.left;
+                    }
             }else{
                 if(root.right!=null && root.right.val==key){
                     root.right=helper(root.right);
@@ -36,8 +36,9 @@ class Solution {
                     root=root.right;
                 }
             }
-         }
-         return dummy;
+            
+        }
+        return dummy;
     }
     public TreeNode helper(TreeNode root){
         if(root.left==null){
@@ -45,18 +46,19 @@ class Solution {
         }
         else if(root.right==null){
             return root.left;
-        }
-        else{
+        }else{
             TreeNode rightchild=root.right;
-            TreeNode lastright=findlastright(root.left);
-            lastright.right=rightchild;
-            return root.left;
+            TreeNode lastrightchild=findminright(root.left);
+            lastrightchild.right=rightchild;
+             return root.left;
         }
+         
+          
     }
-    public TreeNode findlastright(TreeNode root){
+    public TreeNode findminright(TreeNode root){
         if(root.right==null){
             return root;
         }
-        return findlastright(root.right);
+        return findminright(root.right);
     }
 }
