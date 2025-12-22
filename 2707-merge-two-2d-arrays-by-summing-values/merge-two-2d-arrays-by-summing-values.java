f35d8098551499e5eef07586b093747ce7f329f1
nandinigupta-07 <guptanandini072@gmail.com>
2025-12-22 09:13:57 +0530

    Time: 1 ms (87.32%) | Memory: 46.9 MB (29.50%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.java
@@ -1,25 +1,35 @@
 class Solution {
     public int[][] mergeArrays(int[][] nums1, int[][] nums2) {
-        List<int[]> list=new ArrayList<>();
-        for(int i=0;i<nums1.length;i++){
-            list.add(nums1[i]);
-        }
-        for(int i=0;i<nums2.length;i++){
-            list.add(nums2[i]);
-        }
-        Collections.sort(list,(a,b)-> Integer.compare(a[0],b[0]));
-        for(int i=0;i<list.size()-1;i++){
-            if(list.get(i)[0]==list.get(i+1)[0]){
-                list.get(i)[1]=list.get(i)[1]+list.get(i+1)[1];
-                list.remove(i + 1);
-                i--;
+        int i=0;
+        int j=0;
+        List<int[]>list=new ArrayList<>();
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i][0]==nums2[j][0]){
+                list.add(new int [] { nums1[i][0],nums1[i][1]+nums2[j][1]});
+                i++;
+                j++;
+            }
+            else if(nums1[i][0]<nums2[j][0]){
+                list.add(nums1[i]);
+                i++;
+            }
+            else{
+                list.add(nums2[j]);
+                j++;
             }
         }
-        int res[][]=new int[list.size()][2];
-
-        for(int i=0;i<list.size();i++){
-            res[i]=list.get(i);
+        while(i<nums1.length){
+            list.add(nums1[i]);
+            i++;
         }
-        return res;
+        while(j<nums2.length){
+            list.add(nums2[j]);
+            j++;
+        }
+        int re[][]=new int [list.size()][2];
+        for(int k=0;k<list.size();k++){
+            re[k]=list.get(k);
+        }
+        return re;
     }
 }
