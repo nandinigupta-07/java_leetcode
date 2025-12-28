fd43a622a6c343d29474dac43ba490de392b291a
nandinigupta-07 <guptanandini072@gmail.com>
2025-12-28 13:39:44 +0530

    Time: 40 ms (24.82%) | Memory: 59.4 MB (26.01%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,26 +1,26 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        
-        Set<List<Integer>> ans=new HashSet<>();
-         
+         Arrays.sort(nums);
+         Set<List<Integer>> set=new HashSet<>();
          for(int i=0;i<nums.length;i++){
-              
-             if(i > 0 && nums[i] == nums[i-1]) continue;
-             HashSet<Integer>set=new HashSet<>();
-            for(int j=i+1;j<nums.length;j++){
-                  
-                 int k =-(nums[i]+nums[j]);
-                if(set.contains(k)){
-                   ans.add(Arrays.asList(nums[i], k, nums[j]));
-                     
+            if(i>0&& nums[i]==nums[i-1])continue;
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int num=nums[i]+nums[j]+nums[k];
+                if(num<0){
+                    j++;
+                }else if(num>0){
+                    k--;
                 }else{
-                    set.add(nums[j]);
-                     
+                    set.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
+                    while(j<k &&nums[j]==nums[j-1])j++;
+                    while(j<k&&nums[k]==nums[k+1])k--;
                 }
             }
          }
-        return  new ArrayList<>(ans);
-
+        return new ArrayList<>(set);
     }
 }
