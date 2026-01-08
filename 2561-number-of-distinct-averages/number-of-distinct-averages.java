cd25c2f90be65f9a64e1b3f6b329eaa7f4887b21
nandinigupta-07 <guptanandini072@gmail.com>
2026-01-08 11:17:45 +0530

    Time: 4 ms (30.31%) | Memory: 43.2 MB (38.29%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.java
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(nums);
         int i=0;
         int j=nums.length-1;
-        while(i<=j){
+        while(i<j){
             sum=(nums[i]+nums[j])/2.0f;
             set.add(sum);
             i++;
