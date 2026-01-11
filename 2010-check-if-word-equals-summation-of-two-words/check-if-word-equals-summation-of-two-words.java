123f05f7e4909bd65bd0d1b085743cfdd7c10835
nandinigupta-07 <guptanandini072@gmail.com>
2026-01-11 07:44:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (14.57%) - LeetSync

--- 2010-check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.java
+++ 2010-check-if-word-equals-summation-of-two-words/check-if-word-equals-summation-of-two-words.java
@@ -1,26 +1,15 @@
 class Solution {
+    public int convert(String word){
+        int num=0;
+        for(int i=0;i<word.length();i++){
+            num=num*10+(word.charAt(i)-'a');
+        }
+        return num;
+    }
     public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
-          String first="";
-         for(int i=0;i<firstWord.length();i++){
-            char ch=firstWord.charAt(i);
-             first+=ch-'a';
-         }
-         String sec="";
-         for(int i=0;i<secondWord.length();i++){
-            char ch=secondWord.charAt(i);
-             sec+=ch-'a';
-         }
-         String finaal="";
-         for(int i=0;i<targetWord.length();i++){
-            char ch=targetWord.charAt(i);
-             finaal+=ch-'a';
-         }
-         int a=Integer.parseInt(first);
-         int b=Integer.parseInt(sec);
-         int c=Integer.parseInt(finaal);
-         if(a+b==c){
-            return true;
-         }
-         return false;
+         int a=  convert(firstWord);
+         int b=  convert(secondWord);
+         int c=  convert(targetWord);
+         return a+b==c;
     }
 }
