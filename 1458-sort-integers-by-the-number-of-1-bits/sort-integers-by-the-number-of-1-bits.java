a8850425080a344b2d0542e0b3a81d2b1bb00e75
nandinigupta-07 <guptanandini072@gmail.com>
2026-02-25 21:29:14 +0530

    Time: 7 ms (78.21%) | Memory: 46.5 MB (84.05%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Solution {
     public int[] sortByBits(int[] arr) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            if (a[0] == b[0]) return Integer.compare(a[1], b[1]);
-            return Integer.compare(a[0], b[0]);
+            if (a[0] == b[0]) return a[1]-b[1];
+            return a[0]-b[0];
         });
 
         for (int i = 0; i < arr.length; i++) {
